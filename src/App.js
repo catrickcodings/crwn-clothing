0c8eb18bb96150de47bbc29dfd6e8c8221723b5f
Paras Bagri <parasba85@gmail.com>
2024-11-21 23:46:37 +0530

    fiexed scss and many more errors

--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
-import './styles/categories.styles.scss'
+import "./styles/categories.styles.scss";
 const App = () => {
-   const categories = [
-  {
-     id:1,
-     title: 'Hats',
-   },
-   {
-    id:2,
-    title: 'Jackets',
-  },
-  {
-    id:3,
-    title: 'Sneakers',
-  },
-  {
-    id:4,
-    title: 'Womens',
-  },
-  {
-    id:5,
-    title: 'Mens',
-  },
-];
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div classname='categories-container'>
+    <div className="categories-container">
       {categories.map(({ title }) => (
-        <div classname='category-container'>     
-        <div classname='background-image'/>
-       <div classname ="category-body-container">
-        <h2> {title}</h2>
-        <p>Shop Now</p>
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2> {title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
+      ))}
     </div>
-    
-
-
-
-      )
-      )}
-     
-    
-     </div>
   );
 };
-
 export default App;
